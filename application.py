02b114eeb3e5aca960a100ecd143ba48fa780cb6
marcomancini1994 <60708447+marcomancini1994@users.noreply.github.com>
2020-02-25 12:54:56 +0100

    Update and rename app.py to application.py

--- application.py
+++ application.py
@@ -1,15 +1,16 @@
 # import flask dependencies
 from flask import Flask
 # initialize the flask app
-app = Flask(__name__)
+application = Flask(__name__)
 # default route
-@app.route('/)
+@application.route('/)
 def index():
     return 'Hello World!'
 # create a route for webhook
-@app.route('/webhook')
+@application.route('/webhook')
 def hello():
     return 'Hello World!'
+
 # run the app
 if __name__ == '__main__':
-   app.run()
+   application.run()
