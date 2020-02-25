e6cd6dd24faeee6ecf212b9ea54dec809456d394
marcomancini1994 <60708447+marcomancini1994@users.noreply.github.com>
2020-02-25 09:21:22 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
 
-	$requestBody = file_get_contents('https://covid2019-api.herokuapp.com/country/italy');
-	$json = json_decode($requestBody, true);
+    $requestBody = file_get_contents('https://covid2019-api.herokuapp.com/country/italy');
+    $json = json_decode($requestBody, true);
 
     $confirmed = $json['Italy']['confirmed'];
     $death =  $json['Italy']['deaths'];
@@ -10,11 +10,25 @@
     $date = $json['dt'];
     
     $speech = "Ultimo aggiornamento: $date\nNumero casi: $confirmed\nNumero decessi: $death\nNumero guariti: $recovered\nMappa dettagliata: https://bit.ly/38TTenD";
-        
-    $response = new \stdClass();
-	$response->speech = $speech;
-	$response->displayText = $speech;
-	$response->source = "webhook";
-	echo json_encode($response)
+    
+    $resp = '{
+		  "payload": {
+		    "google": {
+		      "expectUserResponse": true,
+		      "richResponse": {
+		        "items": [
+		          {
+		            "simpleResponse": {
+		              "textToSpeech": "'.$speech.'"
+		            }
+		          }
+		        ]
+		      }
+		    }
+		  }
+		}
+	     ';
+	$response = json_encode($resp, true);
+	echo $response;
 
 ?>
